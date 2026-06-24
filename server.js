24074167bbe1f65cdaa618695c00c3f610cac3ac
Alicexiaoshuokeizer <xiaoshuoye@outlook.com>
2026-06-24 19:38:21 +0100

    Debug port setting

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.delete("/tasks/:id", (req, res) => {
   res.status(204).send();
 });
 
-const PORT = procces.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
